5cdf56e90916dca65390c6abaf519996ce5310b2
AndresGarciaVa <andresgarciav901@gmail.com>
2022-09-19 16:32:39 -0500

    primer-commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Todoapp from './components/todoapp'
+
+function App() {
+
+  return (
+    <Todoapp/>
+  )
+}
+
+export default App
---
+++ src/components/todoapp.jsx
@@ -0,0 +1,11 @@
+
+export default function Todoapp(){
+    
+
+    return  <div className="todoContainer">
+            <form className="todoCreateForm">
+                <input className="todoInput"/>
+                <input type="submit" value="Create Todo" className="buttomCreate"/>
+            </form>
+        </div>
+}
